99d6c3dff15b44318405ece54b4f6640bbcb5363
sanshi <zhz961101@gmail.com>
2020-10-16 05:19:56 +0800

    fix mount button.

--- ytp-rotate.user.js
+++ ytp-rotate.user.js
@@ -2,7 +2,7 @@
 // @author          zhzLuke96
 // @name            油管视频旋转
 // @name:en         youtube player rotate plug
-// @version         0.8
+// @version         0.9
 // @description     油管的视频旋转插件.
 // @description:en  rotate youtebe player.
 // @namespace       https://github.com/zhzLuke96/
@@ -190,6 +190,16 @@
         return void 0;
     }
 
+    // rotate 90
+    addbutton(`
+<svg viewBox="0 0 1536 1536" aria-labelledby="rwsi-awesome-repeat-title" id="si-awesome-repeat" width="100%" height="100%"><title id="rwsi-awesome-repeat-title">icon repeat</title><path d="M1536 128v448q0 26-19 45t-45 19h-448q-42 0-59-40-17-39 14-69l138-138Q969 256 768 256q-104 0-198.5 40.5T406 406 296.5 569.5 256 768t40.5 198.5T406 1130t163.5 109.5T768 1280q119 0 225-52t179-147q7-10 23-12 14 0 25 9l137 138q9 8 9.5 20.5t-7.5 22.5q-109 132-264 204.5T768 1536q-156 0-298-61t-245-164-164-245T0 768t61-298 164-245T470 61 768 0q147 0 284.5 55.5T1297 212l130-129q29-31 70-14 39 17 39 59z"></path></svg>
+            `, {
+        click: () => playerApi.rotate && playerApi.rotate(),
+        css: "fill:white;width:20px;margin-right:1rem;",
+        id: "rotate-btn",
+        title: on_zh_lang ? "旋转视频" : "rotate"
+    })
+
     function append_context_menu({
         label = '',
         content = '<div class="ytp-menuitem-toggle-checkbox"></div>',
@@ -213,22 +223,12 @@
         $menu.appendChild($ele)
     };
 
-    (function mountBtnAndMenu() {
+    (function mountMenu() {
         const $menu = document.querySelector(".ytp-contextmenu>.ytp-panel>.ytp-panel-menu");
         if (!$menu || $menu.innerHTML === '') {
             // waiting framework do something
             return setTimeout(mountBtnAndMenu, 300)
         }
-
-        // rotate 90
-        addbutton(`
-            <svg viewBox="0 0 1536 1536" aria-labelledby="rwsi-awesome-repeat-title" id="si-awesome-repeat" width="100%" height="100%"><title id="rwsi-awesome-repeat-title">icon repeat</title><path d="M1536 128v448q0 26-19 45t-45 19h-448q-42 0-59-40-17-39 14-69l138-138Q969 256 768 256q-104 0-198.5 40.5T406 406 296.5 569.5 256 768t40.5 198.5T406 1130t163.5 109.5T768 1280q119 0 225-52t179-147q7-10 23-12 14 0 25 9l137 138q9 8 9.5 20.5t-7.5 22.5q-109 132-264 204.5T768 1536q-156 0-298-61t-245-164-164-245T0 768t61-298 164-245T470 61 768 0q147 0 284.5 55.5T1297 212l130-129q29-31 70-14 39 17 39 59z"></path></svg>
-            `, {
-            click: () => playerApi.rotate && playerApi.rotate(),
-            css: "fill:white;width:20px;margin-right:1rem;",
-            id: "rotate-btn",
-            title: on_zh_lang ? "旋转视频" : "rotate"
-        })
 
         // github link
         append_context_menu({
